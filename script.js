5093cf67301075ae00c972cde2c6559604e7e258
tfcruzBruno <cruz.enriquez.bruno@gmail.com>
2024-10-17 16:46:50 -0600

    hoy 171024

---
+++ script.js
@@ -0,0 +1,12 @@
+document.title = "Clase de JS";
+console.log(document.title);
+
+const h1 = document.createElement("h1");
+h1.textContent = "Hola skibidi amigo";
+
+document.body.appendChild(h1);
+
+const parrafo = document.createElement("p");
+parrafo.textContent = "mi texto desde js omg thats crazy bro what the hell kfhjrhujdbvnhcbhevhd"
+document.body.appendChild(parrafo);
+
